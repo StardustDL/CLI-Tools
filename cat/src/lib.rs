eda422fdd88d7694dfdbb7bee36ee6bb278ea3cd
StardustDL <stardustdl@163.com>
2019-01-16 12:15:35 +0800

    cat: support --number flag

--- cat/src/lib.rs
+++ cat/src/lib.rs
@@ -1,18 +1,36 @@
 use std::fs;
-use std::io;
+use std::io::{self, BufRead, BufReader, Read, Result};
 use std::path;
 
-pub fn run_file(filename: &path::Path) -> io::Result<()> {
-    run_readable(fs::File::open(filename)?)
+pub struct Config<'a> {
+    pub file: Option<&'a str>,
+    pub number: bool,
 }
 
-pub fn run_stdin() -> io::Result<()> {
-    run_readable(io::stdin())
+impl<'a> Config<'a> {
+    pub fn new(args: &'a clap::ArgMatches) -> Self {
+        let file = args.value_of("FILE");
+        let number = args.is_present("number");
+
+        Config { file, number }
+    }
 }
 
-fn run_readable<T: io::Read>(mut item: T) -> io::Result<()> {
-    let mut contents = String::new();
-    item.read_to_string(&mut contents)?;
-    println!("{}", contents);
+pub fn run(config: Config) -> Result<()> {
+    let file: Box<dyn Read> = match config.file {
+        Some(name) if name != "-" => Box::new(fs::File::open(path::Path::new(&name))?),
+        _ => Box::new(io::stdin()),
+    };
+    let reader = BufReader::new(file);
+    for (number,line) in reader.lines().enumerate() {
+        match config {
+            Config { number: true, .. } => {
+                println!("{:4}  {}", number + 1, line?);
+            }
+            _ => {
+                println!("{}", line?);
+            }
+        }
+    }
     Ok(())
 }
--- cat/src/main.rs
+++ cat/src/main.rs
@@ -8,19 +8,9 @@ use clap::App;
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
-
-    match matches.value_of("FILE") {
-        Some(filename) => {
-            run_file(std::path::Path::new(filename)).unwrap_or_else(|err| {
-                println!("Error(s): {}", err);
-                process::exit(1);
-            });
-        }
-        None => {
-            run_stdin().unwrap_or_else(|err| {
-                println!("Error(s): {}", err);
-                process::exit(1);
-            });
-        }
-    }
+    let config = Config::new(&matches);
+    run(config).unwrap_or_else(|err| {
+        println!("Error(s): {:?}",err);
+        process::exit(1);
+    })
 }
