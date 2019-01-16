8bba6b0fa0201efdeb9c304cc99d29e54a1a8b19
StardustDL <stardustdl@163.com>
2019-01-16 15:51:31 +0800

    cat: support multiple files

--- cat/src/lib.rs
+++ cat/src/lib.rs
@@ -3,7 +3,7 @@ use std::io::{self, BufRead, BufReader, Read, Result};
 use std::path;
 
 pub struct Config<'a> {
-    pub file: Option<&'a str>,
+    pub file: Option<clap::Values<'a>>,
     pub number: bool,
     pub number_nonblank: bool,
     pub show_ends: bool,
@@ -14,7 +14,7 @@ pub struct Config<'a> {
 
 impl<'a> Config<'a> {
     pub fn new(args: &'a clap::ArgMatches) -> Self {
-        let file = args.value_of("FILE");
+        let file = args.values_of("FILE");
         let number = args.is_present("number");
         let number_nonblank = args.is_present("number-nonblank");
         let show_all = args.is_present("show-all");
@@ -37,49 +37,48 @@ impl<'a> Config<'a> {
 }
 
 pub fn run(config: Config) -> Result<()> {
-    let file: Box<dyn Read> = match config.file {
-        Some(name) if name != "-" => Box::new(fs::File::open(path::Path::new(&name))?),
-        _ => Box::new(io::stdin()),
+    let files = match config.file {
+        Some(items) => items.collect(),
+        None => vec!["-"],
     };
+    for item in files {
+        let file: Box<dyn Read> = match item {
+            "-" => Box::new(io::stdin()),
+            _ => Box::new(fs::File::open(path::Path::new(item))?),
+        };
 
-    let reader = BufReader::new(file);
-    let mut number_nonblank = 0;
-    let mut last_empty = false;
-    for (number, line_raw) in reader.lines().enumerate() {
-        let mut line = line_raw?;
-        let is_empty = line.is_empty();
-        if is_empty && last_empty && config.squeeze_blank {
-            continue;
-        }
-        if config.show_tabs {
-            line = line.replace("\t", "^I");
-        }
-        if config.show_nonprinting {
-            line = line.replace("\r", "^M");
-        }
-        if config.show_ends {
-            line.push_str("$");
-        }
-        match config {
-            Config {
-                number_nonblank: true,
-                ..
-            } => {
+        let reader = BufReader::new(file);
+        let mut number_nonblank = 0;
+        let mut last_empty = false;
+        for (number, line_raw) in reader.lines().enumerate() {
+            let mut line = line_raw?;
+            let is_empty = line.is_empty();
+            if is_empty && last_empty && config.squeeze_blank {
+                continue;
+            }
+            if config.show_tabs {
+                line = line.replace("\t", "^I");
+            }
+            if config.show_nonprinting {
+                line = line.replace("\r", "^M");
+            }
+            if config.show_ends {
+                line.push_str("$");
+            }
+            if config.number_nonblank {
                 if is_empty {
                     println!("{:4}  {}", "", line);
                 } else {
                     number_nonblank += 1;
                     println!("{:4}  {}", number_nonblank, line);
                 }
-            }
-            Config { number: true, .. } => {
+            } else if config.number {
                 println!("{:4}  {}", number + 1, line);
-            }
-            _ => {
+            } else {
                 println!("{}", line);
             }
+            last_empty = is_empty;
         }
-        last_empty = is_empty;
     }
 
     Ok(())
--- cat/src/main.rs
+++ cat/src/main.rs
@@ -7,11 +7,15 @@ use clap::App;
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
-    let matches = App::from_yaml(yaml).get_matches();
+    let matches = App::from_yaml(yaml)
+        .name(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!("\n"))
+        .get_matches();
     let config = Config::new(&matches);
-    
+
     run(config).unwrap_or_else(|err| {
-        println!("Error(s): {:?}",err);
+        println!("Error(s): {:?}", err);
         process::exit(1);
     })
 }
